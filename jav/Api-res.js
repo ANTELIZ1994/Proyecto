9e3b407840eec22b1411b4b40e1767f97aa3b7a1
ANTELIZ1994 <anteliz.albeiro1994@gmail.com>
2025-02-06 20:07:40 -0500

    cambios de API

--- jav/Api-res.js
+++ jav/Api-res.js
@@ -1,4 +1,32 @@
-const apiURL= 'https://restcountries.com/v3.1/region/North America';
+const apiURL= 'https://restcountries.com/v3.1/region/north america';
 function API(){
-  alert("El pais que quiero visitar es CANADA");
+async function API(){
+
+  try{
+
+    const resultsDiv=document.getElementById('resultado');
+    console.resultado="el pais que quiero conocer es" + resultado;
+
+    const response=await fetch(apiURL);
+    if(!response.ok){
+         throw new Error("Network response was not ok");
+
+    }
+    const users=await response.json();
+     datos="";
+    users.forEach(element => {
+      datos=datos+`${element.id}+"-"+${element.username}`
+      
+    });
+    resultsDiv.innerHTML=datos;
+    
+
+  }catch (error){
+    console.error("hubo error",error);
+  }
+  
+
+
+
+}
 }
